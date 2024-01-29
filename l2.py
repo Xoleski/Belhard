34447daf5709ce8d5d38e6a311359229c3b2f3da
fxnatkx <dantorik.661@gmail.com>
2024-01-29 19:50:22 +0300

    sing

--- l2.py
+++ l2.py
@@ -1,4 +1,21 @@
 print(3, 44, "Yellow", sep="-|-")
 
-text = input("Enter some text")
-print(text)
+#text = input("Enter some text")
+# print(text)
+
+
+fin = None
+
+cinv = -3.14159
+fdr = round(cinv, 2)
+print(fdr)
+
+dft = abs(fdr)
+print(fdr)
+
+
+
+a = "23FFF"
+print(int(a, 16))
+
+
