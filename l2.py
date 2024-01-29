c9fe91ee65971cc02b3ae24d2a2e2d3a6ea45dd4
fxnatkx <dantorik.661@gmail.com>
2024-01-29 19:58:44 +0300

    sing

--- l2.py
+++ l2.py
@@ -1,21 +1,38 @@
-print(3, 44, "Yellow", sep="-|-")
+# print(3, 44, "Yellow", sep="-|-")
+#
+# #text = input("Enter some text")
+# # print(text)
+#
+#
+# fin = None
+#
+# cinv = -3.14159
+# fdr = round(cinv, 2)
+# print(fdr)
+#
+# dft = abs(fdr)
+# print(fdr)
+#
+#
+#
+# a = "FFF"
+# print(int(a, 16))
+#
+# yr = 126
+# print(bin(yr))
+# print(hex(yr))
+# print(oct(yr))
+#
+# print(
+#     bool(5244522)
+# )
+#
+# print(bool(""))
+#
+# print(bool(" "))
 
-#text = input("Enter some text")
-# print(text)
-
-
-fin = None
-
-cinv = -3.14159
-fdr = round(cinv, 2)
-print(fdr)
-
-dft = abs(fdr)
-print(fdr)
 
 
 
-a = "23FFF"
-print(int(a, 16))
 
 
