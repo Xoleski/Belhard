f7e1da4fd8fc2e39627a2ea9035004cb8e6f15db
Xoleski <xoleski@proton.me>
2024-01-30 16:56:53 +0300

    changes

--- Lesson_03/Task_01.py
+++ Lesson_03/Task_01.py
@@ -2,4 +2,13 @@
 # заменить все пробелы на "-"
 # 2-мя способами
 
+input_line = input("Напишите предложение...")
+print(input_line)
+
+
+chang_line = input_line.replace(" ", "-")
+print(chang_line)
+
+
+
 
