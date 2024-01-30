0abda4820fd09c8d331c9a3038430ebb2de41434
Xoleski <xoleski@proton.me>
2024-01-30 18:40:49 +0300

    done

--- Lesson_03/Task_02.py
+++ Lesson_03/Task_02.py
@@ -2,5 +2,16 @@
 # найти среднее арифмитическое
 # с точностью 3
 
+numbers_list = input("Введите три числа через пробел: ")
+print(numbers_list)
+
+arithmetic_mean = ((float(numbers_list.split()[0])
+                   + float(numbers_list.split()[1])
+                   + float(numbers_list.split()[2]))
+                   / 3)
+
+print(arithmetic_mean)
+
+
 
 
