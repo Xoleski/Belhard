c61113b6e4cfcd0d7cfa32fa8ae4e80cbbce187a
fxnatkx <dantorik.661@gmail.com>
2024-01-31 19:09:25 +0300

    fix

--- Lesson_03/Task_04.py
+++ Lesson_03/Task_04.py
@@ -9,7 +9,7 @@ number_2 = float(number_input.split()[1]) > 0
 number_3 = float(number_input.split()[2]) > 0
 
 true_count = number_1 + number_2 + number_3
-false_count = 3 - true_count
+false_count = 3 - true_count # нужно через знаки, потому что ноль
 
 print(f"\nКоличество положительных чисел = {true_count}.")
 print(f"Количество отрицательных чисел = {false_count}.")
