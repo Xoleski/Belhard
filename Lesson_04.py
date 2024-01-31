8ff4b5b42aa2f81b02fc2bebeb888e78e6d677a7
fxnatkx <dantorik.661@gmail.com>
2024-01-31 19:45:32 +0300

    com

--- Lesson_04.py
+++ Lesson_04.py
@@ -1,1 +1,44 @@
+# obj = [1, "H", 5]
+#
+# # text = "Hello"
+# # print(list(text))
+#
+# obj[1] = "First"
+# print(obj)
+#
+# obj[1:3] = "fir"
+# print(obj)
+
+# zeros = [0 for i in range(10)] # для и в диапазоне от 0 до 100
+# print(zeros)
+text = "a"
+obj = [1, "H", 5, text]
+
+# ht = obj.pop(2)
+# print(ht)
+#
+# obj2 = obj[0:2]
+# del obj2[0:2]
+#
+#
+# obj.remove(1) # удаляет по значению
+
+gtr = [2, 5, 1]
+# gtr.append(5) # один объект в конеу
+#
+# gtr.insert(0, 7)
+#
+
+# gtr.extend([5, 6, 7])
+
+print(gtr)
+
+
+# frt = gtr + gtr
+#
+# print(frt)
+
+
+
+
 
