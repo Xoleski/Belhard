ac752380022621c96951192f193c5e29311c872e
fxnatkx <dantorik.661@gmail.com>
2024-02-05 19:59:30 +0300

    com

--- Lesson_05.py
+++ Lesson_05.py
@@ -1,1 +1,52 @@
+a = 5
+
+# if a > 0:
+#     print("a is pos")
+#
+# elif a == 0:
+#     print("zero")
+# else:
+#     print("a is not ps")
+#
+# b=0
+# b is None
+#
+# hum = False
+# if not False
+
+# if  - нужно привести к True
+
+
+# print(isinstance(123.555, (int, float)))
+# print(isinstance(123.555, int | float)) # python 3.10
+
+
+# city = None
+# user = {}
+# user["city"] = city or "Mogilev" # or выберет первый, если первый True
+# print(user)
+#
+# city = None
+# user = {}
+# user["city"] = city and "Mogilev" # end выберет первый, если первый False
+# print(user)
+
+text = "Hello world"
+for i in text:
+    print(i.upper())
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
 
