70cc08d0291ff01d4d0c00b27453c395baf58ab5
Xoleski <xoleski@proton.me>
2024-02-06 15:15:35 +0300

    done

--- Lesson_05/Task_4.py
+++ Lesson_05/Task_4.py
@@ -1,1 +1,25 @@
+# Пользователь вводит химическую формулу (элементы однобуквенныe и формула без скобок)
+# Подсчитать количество молекул
+# input: C2H5OH
+# output: {"C": 2, "H": 6, "O": 1}
+
+formula = "C2H5OH"
+k = list(formula)
+
+g =[]
+
+i = 0
+
+while i < len(k):
+    if k[i].isalpha():
+        g.extend(list(k[i]))
+    elif k[i].isdigit() and k[i]:
+        g.extend(list(k[i-1]*(int(k[i])-1)))
+    i += 1
+
+kor = {g[i]: g.count(g[i]) for i in range(len(g))}
+print(kor)
+
+
+
 
