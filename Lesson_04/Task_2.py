623b0b7c6fb7d3bb53ab565aa02a9d9cc285283c
Xoleski <xoleski@proton.me>
2024-02-06 09:36:16 +0300

    corrected

--- Lesson_04/Task_2.py
+++ Lesson_04/Task_2.py
@@ -4,10 +4,11 @@
 
 fraza = input("Введите любую фразу: ")
 
-buk = sorted(set(fraza))
+# buk = sorted(set(fraza))
 
-slovar = {buk[i]: fraza.count(buk[i]) for i in range(len(buk))}
+# slovar = {buk[i]: fraza.count(buk[i]) for i in range(len(buk))}
 
+slovar = {fraza[i]: fraza.count(fraza[i]) for i in range(len(fraza))}
 print(slovar)
 
 # просто в словарь из текста. при повторе буквы заменит значение
