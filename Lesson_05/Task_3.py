69ba2cc4fde8a8b55f9377c07de6c67995e586b6
Xoleski <xoleski@proton.me>
2024-02-06 13:34:26 +0300

    done

--- Lesson_05/Task_3.py
+++ Lesson_05/Task_3.py
@@ -1,1 +1,12 @@
+# Вывести четные числа от 2 до N по 5 в строку
 
+n = 155
+
+lis = [i for i in range(2, n) if not i % 2]
+
+d = 0
+
+while d < len(lis):
+    if len(lis) > 5:
+        print(lis[d:(d+5)])
+    d += 5
