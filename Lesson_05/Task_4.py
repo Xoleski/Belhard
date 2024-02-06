c42640ba4799f2937c473163979cfb4dbba2a0f9
Xoleski <xoleski@proton.me>
2024-02-06 15:37:07 +0300

    done

--- Lesson_05/Task_4.py
+++ Lesson_05/Task_4.py
@@ -3,7 +3,7 @@
 # input: C2H5OH
 # output: {"C": 2, "H": 6, "O": 1}
 
-formula = "C2H5OH"
+formula = "C12H22O11"
 k = list(formula)
 
 g =[]
@@ -13,7 +13,9 @@ i = 0
 while i < len(k):
     if k[i].isalpha():
         g.extend(list(k[i]))
-    elif k[i].isdigit() and k[i]:
+    elif k[i-1].isdigit() and k[i].isdigit():
+        g.extend(list(k[i-2]*(int(k[i-1]+k[i])-1)))
+    elif k[i].isdigit() and k[i+1].isalpha():
         g.extend(list(k[i-1]*(int(k[i])-1)))
     i += 1
 
