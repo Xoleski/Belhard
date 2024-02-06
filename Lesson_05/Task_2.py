6016cc9236efe684e7ee36e413aee7c8a5b217fd
Xoleski <xoleski@proton.me>
2024-02-06 23:56:38 +0300

    done

--- Lesson_05/Task_2.py
+++ Lesson_05/Task_2.py
@@ -18,9 +18,45 @@ try:
 
     print(f"РЕЗУЛЬТАТ = {act[c]}")
 
+
 except ZeroDivisionError as exc:
     print("ОШИБКА! Делить на ноль нельзя!")
 except ValueError as exc:
     print("ОШИБКА! Нужно вводить числа цифрами!")
 except KeyError as exc:
     print("ОШИБКА! Допустимый ввод действий: +, -, *, /, **, %!")
+
+
+
+## ВАРИАНТ С ЦИКЛАМИ
+
+# er_1 = "ОШИБКА! Делить на ноль нельзя!"
+# er_2 = "ОШИБКА! Допустимый ввод действий: +, -, *, /, **, %!"
+# er_3 = "ОШИБКА! Нужно вводить числа цифрами!"
+#
+# try:
+#     a1 = float(input("Введите первое число: "))
+#     c1 = input("Введите действие (+, -, *, /, **, %): ")
+#     b1 = float(input("Введите второе число: "))
+#
+#     if c1 == "+":
+#         rez = a1 + b1
+#     elif c1 == "-":
+#         rez = a1 - b1
+#     elif c1 == "*":
+#         rez = a1 * b1
+#     elif c1 == "/":
+#         if not b1:
+#             rez = er_1
+#         else:
+#             rez = a1 / b1
+#     elif c1 == "**":
+#         rez = a1 ** b1
+#     elif c1 == "%":
+#         rez = a1 % b1
+#     else:
+#         rez = er_2
+#     print(f"РУЗУЛЬТАТ = {rez}" if not (rez == er_1 or rez == er_2) else f"{rez}")
+#
+# except ValueError as exc:
+#     print(er_3)
