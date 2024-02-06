44dd928756e4252afdaad0805bdb5826b99b8326
Xoleski <xoleski@proton.me>
2024-02-06 11:44:15 +0300

    done

--- Lesson_05/Task_1.py
+++ Lesson_05/Task_1.py
@@ -1,1 +1,14 @@
+# Вывести первые N чисел кратные M и больше K
 
+numb = []
+n = 5
+m = 5
+k = -5
+
+while len(numb) < n:
+
+    if not (k+1) % m:
+        numb.append(k+1)
+    k += 1
+
+print(numb)
