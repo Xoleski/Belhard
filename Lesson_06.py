25da079ea575db99cd8d65068afbd732ebf84266
fxnatkx <dantorik.661@gmail.com>
2024-02-07 19:51:41 +0300

    initial commit

--- Lesson_06.py
+++ Lesson_06.py
@@ -1,1 +1,18 @@
+# def length(a):
+#     cout = 0
+#
+#
+#     return
+
+
+objs = [3, 5, "4", 6, "-23", -45]
+
+objs.sort(key=lambda x: x if isinstance(int, float) else int(x))
+
+print(objs)
+
+
+
+
+
 
