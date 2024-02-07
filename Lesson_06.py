afe02f5de83bfca7c3251bdf6aeec8336c5f4d54
fxnatkx <dantorik.661@gmail.com>
2024-02-07 21:35:42 +0300

    initial commit

--- Lesson_06.py
+++ Lesson_06.py
@@ -5,11 +5,14 @@
 #     return
 
 
-objs = [3, 5, "4", 6, "-23", -45]
+# objs = [3, 5, "4", 6, "-23", -45]
+#
+# objs.sort(key=lambda x: x if isinstance(int, float) else int(x))
+#
+# print(objs)
+#
+# def foo(a: list) -> bool:
 
-objs.sort(key=lambda x: x if isinstance(int, float) else int(x))
-
-print(objs)
 
 
 
