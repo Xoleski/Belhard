624140d0ec0f7a2257c3341cf2db6f4f9caa27e8
Xoleski <xoleski@proton.me>
2024-02-08 22:28:54 +0300

    half

--- Lesson_06/Task_1.py
+++ Lesson_06/Task_1.py
@@ -1,1 +1,24 @@
+# Написать функцию перевода десятичного числа в двоичное и обратно, без
+# использования функции int
+
+
+def gh(numb):
+    k = []
+    v = numb
+    while v:
+
+        a = v % 2
+        k.append(a)
+        v = v // 2
+
+    k.reverse()
+    l = "".join(list(map(str, k)))
+
+    return l
+
+
+a = gh(16)
+print(a)
+
+
 
