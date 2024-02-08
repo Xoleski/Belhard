75aa3bb78d0fb86ef030da0e2084e37a2307851d
Xoleski <xoleski@proton.me>
2024-02-08 23:15:22 +0300

    done

--- Lesson_06/Task_1.py
+++ Lesson_06/Task_1.py
@@ -2,23 +2,70 @@
 # использования функции int
 
 
-def gh(numb):
-    k = []
-    v = numb
-    while v:
+# def bin_in(numb: int):
+#     k = []
+#     v = numb
+#     while v:
+#
+#         a = v % 2
+#         k.append(a)
+#         v = v // 2
+#
+#     k.reverse()
+#     l = "".join(list(map(str, k)))
+#
+#     return l
+#
+#
+# a = bin_in(20)
+# print(a)
+#
+#
+# def bin_out(code: str):
+#     i = 0
+#
+#     while bin_in(i) != code:
+#         i += 1
+#
+#     return i
+#
+#
+# b = bin_out("10100")
+# print(b)
 
-        a = v % 2
-        k.append(a)
-        v = v // 2
 
-    k.reverse()
-    l = "".join(list(map(str, k)))
+def vb(fra):
+        def bin_in(fra):
+            k = []
+            v = fra
+            while v:
 
-    return l
+                a = v % 2
+                k.append(a)
+                v = v // 2
+
+            k.reverse()
+            l = "".join(list(map(str, k)))
+
+            return l
+
+        def bin_out(fra):
+            i = 0
+
+            while bin_in(i) != fra:
+                i += 1
+
+            return i
+
+        if isinstance(fra, int):
+            return bin_in(fra)
+        else:
+            return bin_out(fra)
 
 
-a = gh(16)
-print(a)
 
+
+print(a := vb(13))
+print(b := vb("100101"))
 
 
