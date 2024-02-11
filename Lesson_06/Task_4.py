3782afc958b9e0d760291f049dc6552df670aff0
Xoleski <xoleski@proton.me>
2024-02-11 16:47:51 +0300

    done

--- Lesson_06/Task_4.py
+++ Lesson_06/Task_4.py
@@ -1,1 +1,11 @@
+# Дан список содержащий в себе различные типы данных, отфильтровать таким
+# образом, чтобы остались только строки, использование дополнительного списка
+# незаконно
+
+def sort_list(list1: list):
+    return list(filter(lambda x: isinstance(x, str), list1))
+
+
+ls1 = ["ghf", 7, {4, "dfkj"}, "", None, False, 9, "world", ]
+print(sort_list(ls1))
 
