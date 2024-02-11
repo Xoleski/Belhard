c27d6c2e808eb587fa0e3f5edcd33cdcf161efcd
Xoleski <xoleski@proton.me>
2024-02-11 17:15:25 +0300

    done

--- Lesson_06/Task_6.py
+++ Lesson_06/Task_6.py
@@ -1,1 +1,16 @@
+# Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
+# четные, потом нечётные
 
+def sort_chet_l(list_b: list):
+    list_b = (list(filter(lambda x: not x % 2, list_b))
+              + list(filter(lambda x: x % 2, list_b)))
+    b = list(filter(lambda x: not x, list_b))
+    i = 0
+    while i < len(b):
+        list_b.remove(b[i])
+        i += 1
+    return list_b
+
+
+ch_nch = [85, 87, 96, 24, 50, 0, 0]
+print(sort_chet_l(ch_nch))
