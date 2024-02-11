c79e2fdf87d9be5a0a45213d0923fb80a7d063ba
Xoleski <xoleski@proton.me>
2024-02-11 15:59:25 +0300

    done

--- Lesson_06/Task_1.py
+++ Lesson_06/Task_1.py
@@ -1,39 +1,6 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без
 # использования функции int
 
-
-# def bin_in(numb: int):
-#     k = []
-#     v = numb
-#     while v:
-#
-#         a = v % 2
-#         k.append(a)
-#         v = v // 2
-#
-#     k.reverse()
-#     l = "".join(list(map(str, k)))
-#
-#     return l
-#
-#
-# a = bin_in(20)
-# print(a)
-#
-#
-# def bin_out(code: str):
-#     i = 0
-#
-#     while bin_in(i) != code:
-#         i += 1
-#
-#     return i
-#
-#
-# b = bin_out("10100")
-# print(b)
-
-
 def vb(fra):
         def bin_in(fra):
             k = []
@@ -63,9 +30,6 @@ def vb(fra):
             return bin_out(fra)
 
 
-
-
 print(a := vb(13))
 print(b := vb("100101"))
-
 
