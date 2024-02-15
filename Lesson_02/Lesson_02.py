593ec0d99fae100c7d2ab2d6220e50be0597f40d
Xoleski <xoleski@proton.me>
2024-02-15 23:49:09 +0300

    done

--- Lesson_02/Lesson_02.py
+++ Lesson_02/Lesson_02.py
@@ -1,4 +1,15 @@
 data = "строка в байты"
 print(data.encode())
+print((data_d := data.encode()).decode())
 data_e = "string byte"
 print(data_e.encode())
+
+a = [1, 2**2]
+b = [1, 2*2]
+x = 2*2
+y = 8/2
+
+if x == y:
+    print(True)
+else:
+    print(0)
