7f1aff0a7767721455f9ba1cdddb6d66d164dd42
fxnatkx <dantorik.661@gmail.com>
2024-02-21 21:16:59 +0300

    chg

--- Lesson_10.py
+++ Lesson_10.py
@@ -1,3 +1,13 @@
 # модуль - это пайтон файл
 # файл с модулями - это пакет
 
+
+from sqlite3 import connect
+
+conn = connect("bd.sqlite3")
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY
+    );
+""")
