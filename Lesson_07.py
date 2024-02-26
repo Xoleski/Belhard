d886864a48bd97d26fc8d5af4d35134f1d2c4276
Xoleski <xoleski@proton.me>
2024-02-26 22:09:14 +0300

    done

--- Lesson_07.py
+++ Lesson_07.py
@@ -1,8 +1,19 @@
 class User:
-    @classmethod
 
-    def __init__(self, name):
+    def __init__(self, name, email, password):
         self.name = name
+        self.email = email
+        self.password = password
+        self.is_active = False
 
+    def info(self):
+        return f"name = {self.name} email = {self.email} is_active = {self.is_active}"
+
+user1 = User(name="Vasya", email="kfd@dkj", password="hh")
+
+print(user1.name)
+
+user3 = User(name="Petay", email="hjhh@khgg", password="hfjh")
+print(user3.info())
 
 
