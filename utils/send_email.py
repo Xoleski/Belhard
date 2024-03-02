612d96c10bf7cb555c076a30d929356cfe9b0797
Xoleski <xoleski@proton.me>
2024-03-02 18:59:04 +0300

    done

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,2 +1,14 @@
-def send_email(email):
-    ...
+
+
+__all__ = [
+    "send_email"
+]
+
+
+def send_email(subject: str, message: str, email: str, recipient: list):
+    print("SEND")
+
+
+# ТОЧКА ВХОДА
+if __name__ == '__main__':
+    send_email("", "", "", [])
