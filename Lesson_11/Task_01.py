2b7fbf5f9e36fdcfe761680f8dd38ff2387d17ab
Xoleski <xoleski@proton.me>
2024-03-03 18:32:30 +0300

    done

--- Lesson_11/Task_01.py
+++ Lesson_11/Task_01.py
@@ -3,7 +3,7 @@ from psycopg2._psycopg import cursor
 from psycopg2.extras import NamedTupleCursor
 
 with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor):
-    ...
+    with conn.cursor() as
 
 
 
