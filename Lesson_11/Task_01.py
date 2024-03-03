7ea397a586b770066b939037c37060a12f1736dd
Xoleski <xoleski@proton.me>
2024-03-03 18:58:58 +0300

    done

--- Lesson_11/Task_01.py
+++ Lesson_11/Task_01.py
@@ -9,16 +9,24 @@ with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_
             CREATE TABLE IF NOT EXIST departments(
             id SERIAL NOT NULL PRIMARY KEY,
             name VARCHAR(32) NOT NULL UNIQUE CHECK ( length(name) >= 2 )
-            )
-            
+            );
+        """)
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS sub_departments(
+            id SERIAL NOT NULL PRIMARY KEY,
+            name VARCHAR(32) NOT NULL UNIQUE CHECK ( lenght(name) >= 2 )
+            );
         """)
 
         cur.execute("""
             CREATE TABLE IF NOT EXIST users(
             id SERIAL NOT NULL PRIMARY KEY,
+            department_id  INTEGER,
+            sub_department_id INTEGER,
             FOREIGN KEY (department_id) REFERENCE departments(id) ON DELETE RESTRICT ON UPDATE CASCADE, 
-            FOREIGN KEY (sub_department_id)
-            )
+            FOREIGN KEY (sub_department_id) REFERENCE sub_departments(id) ON DELETE RESTRICT ON UPDATE CASCADE
+            );
         """)
 
 
