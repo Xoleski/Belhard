2b0a258ea6f2111e741e012c993606be6b1fef07
Xoleski <xoleski@proton.me>
2024-03-03 18:36:41 +0300

    done

--- Lesson_11/Task_01.py
+++ Lesson_11/Task_01.py
@@ -5,7 +5,8 @@ from psycopg2.extras import NamedTupleCursor
 with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
     with conn.cursor() as cur:
         cur.execute("""
-            CREATE TABLE IF NOT EXIST 
+            CREATE TABLE IF NOT EXIST users(
+            id INTEGER
         """)
 
 
