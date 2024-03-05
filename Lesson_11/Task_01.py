5633b820f20275069ec1412293a19ea2234dd6d0
Xoleski <xoleski@proton.me>
2024-03-05 12:11:51 +0300

    done

--- Lesson_11/Task_01.py
+++ Lesson_11/Task_01.py
@@ -175,30 +175,30 @@ with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_
         # # conn.commit()
         #
 
-        # cur.execute("""
+        # cur.execute("""
 
-        #     SELECT chats.name as chat_name, users.id as user_id
-        #     FROM (
-        #             SELECT *
-        #             FROM chats_relations
-        #             INNER JOIN departments ON departments.id = chats_relations.department_id AND chats_relations.sub_department_id IS NULL
+        #     SELECT chats.name as chat_name, users.id as user_id
+        #     FROM (
+        #             SELECT *
+        #             FROM chats_relations
+        #             INNER JOIN departments ON departments.id = chats_relations.department_id AND chats_relations.sub_department_id IS NULL
 
-        #             UNION
-        #             SELECT *
-        #             FROM chats_relations
-        #             INNER JOIN sub_departments ON sub_departments.id = chats_relations.sub_department_id AND chats_relations.department_id IS NULL
+        #             UNION
+        #             SELECT *
+        #             FROM chats_relations
+        #             INNER JOIN sub_departments ON sub_departments.id = chats_relations.sub_department_id AND chats_relations.department_id IS NULL
 
-        #             UNION
-        #             SELECT *
-        #             FROM chats_relations
-        #             INNER JOIN departments ON departments.id = chats_relations.department_id
-        #             INNER JOIN sub_departments ON sub_departments.id = chats_relations.sub_department_id
+        #             UNION
+        #             SELECT *
+        #             FROM chats_relations
+        #             INNER JOIN departments ON departments.id = chats_relations.department_id
+        #             INNER JOIN sub_departments ON sub_departments.id = chats_relations.sub_department_id
 
-        #             ) filtered_chats_relations
-        #     INNER JOIN chats ON chats.id = filtered_chats_relations.chat_id
-        #     INNER JOIN users ON users.department_id = filtered_chats_relations.department_id AND users.sub_department_id = filtered_chats_relations.sub_department_id
+        #             ) filtered_chats_relations
+        #     INNER JOIN chats ON chats.id = filtered_chats_relations.chat_id
+        #     INNER JOIN users ON users.department_id = filtered_chats_relations.department_id AND users.sub_department_id = filtered_chats_relations.sub_department_id
 
-        # """)
+        # """)
 
         cur.execute("""
 
@@ -231,68 +231,68 @@ with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_
 # ВТОРОЕ ЗАДАНИЕ
 
 
-with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
-    with conn.cursor() as cur: # type: cursor
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS statuses(
-        #         id SERIAL PRIMARY KEY,
-        #         name VARCHAR(10) UNIQUE CHECK ( length(name) >= 2 )
-        #     );
-        # """)
+with connect(dsn="postgres://user12:___:6666/user12", cursor_factory=NamedTupleCursor) as conn:
+    with conn.cursor() as cur: # type: cursor
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS statuses(
+        #         id SERIAL PRIMARY KEY,
+        #         name VARCHAR(10) UNIQUE CHECK ( length(name) >= 2 )
+        #     );
+        # """)
 
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS users_prod(
-        #         id SERIAL PRIMARY KEY,
-        #         name VARCHAR(24) CHECK ( length(name) >= 2 ),
-        #         email VARCHAR(24) UNIQUE CHECK ( length(name) >= 2 )
-        #     );
-        # """)
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS users_prod(
+        #         id SERIAL PRIMARY KEY,
+        #         name VARCHAR(24) CHECK ( length(name) >= 2 ),
+        #         email VARCHAR(24) UNIQUE CHECK ( length(name) >= 2 )
+        #     );
+        # """)
 
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS categories(
-        #         id SERIAL PRIMARY KEY,
-        #         name VARCHAR(24) UNIQUE CHECK ( length(name) >= 2 )
-        #     );
-        # """)
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS categories(
+        #         id SERIAL PRIMARY KEY,
+        #         name VARCHAR(24) UNIQUE CHECK ( length(name) >= 2 )
+        #     );
+        # """)
 
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS orders(
-        #         id SERIAL PRIMARY KEY,
-        #         user_prod_id INTEGER,
-        #         status_id INTEGER,
-        #         FOREIGN KEY (user_prod_id) REFERENCES users_prod(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-        #         FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE RESTRICT ON UPDATE CASCADE
-        #     );
-        # """)
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS orders(
+        #         id SERIAL PRIMARY KEY,
+        #         user_prod_id INTEGER,
+        #         status_id INTEGER,
+        #         FOREIGN KEY (user_prod_id) REFERENCES users_prod(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        #         FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE RESTRICT ON UPDATE CASCADE
+        #     );
+        # """)
 
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS products(
-        #         id SERIAL PRIMARY KEY,
-        #         title VARCHAR(36) CHECK ( length(title) >= 2 ),
-        #         description VARCHAR(140) CHECK ( length(description) >= 2 ),
-        #         category_id INTEGER,
-        #         FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT ON UPDATE CASCADE
-        #     );
-        # """)
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS products(
+        #         id SERIAL PRIMARY KEY,
+        #         title VARCHAR(36) CHECK ( length(title) >= 2 ),
+        #         description VARCHAR(140) CHECK ( length(description) >= 2 ),
+        #         category_id INTEGER,
+        #         FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT ON UPDATE CASCADE
+        #     );
+        # """)
 
-        # cur.execute("""
-        #     CREATE TABLE IF NOT EXISTS order_items(
-        #         id SERIAL PRIMARY KEY,
-        #         order_id INTEGER,
-        #         product_id INTEGER,
-        #         FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-        #         FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE RESTRICT ON UPDATE CASCADE
-        #     );
-        # """)
+        # cur.execute("""
+        #     CREATE TABLE IF NOT EXISTS order_items(
+        #         id SERIAL PRIMARY KEY,
+        #         order_id INTEGER,
+        #         product_id INTEGER,
+        #         FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        #         FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE RESTRICT ON UPDATE CASCADE
+        #     );
+        # """)
 
-        # conn.commit()
+        # conn.commit()
 
-        cur.execute("""
-            SELECT * FROM users_prod
-        """)
+        cur.execute("""
+            SELECT * FROM users_prod
+        """)
 
-        for result in cur:
-            print(result)
+        for result in cur:
+            print(result)
 
 
 
