f0cadd9b8bafce90ec176e4a20b429c81a048012
fxnatkx <dantorik.661@gmail.com>
2024-03-06 19:24:11 +0300

    chg

--- Lesson_12/Task.py
+++ Lesson_12/Task.py
@@ -18,59 +18,59 @@ from enum import Enum
 
 from sqlalchemy.orm import DeclarativeBase, relationship, sessionmaker
 
-
-class Base(DeclarativeBase):
-    ...
-
-
-class Projects(Base):
-    __tablename__ = "projects"
-    __table_args__ = (
-        CheckConstraint("length(name) >= 2"),
-    )
-
-    id = Column(INT, primary_key=True)
-    name = Column(VARCHAR(length=32), nullable=False, unique=True)
-
-    tasks = relationship(argument="Tasks", back_populates="projects")
-
-
-class UsersW(Base):
-    __tablename__ = "usersw"
-
-    id = Column(INT, primary_key=True)
-
-    tasks = relationship(argument="Tasks", back_populates="usersw")
-
-
-class StatusEnum(Enum):
-    done = "done"
-    not_done = "not_done"
-
-
-class Tasks(Base):
-    __tablename__ = "tasks"
-    __table_args__ = (
-        CheckConstraint("length(title) >= 2"),
-        CheckConstraint("length(description) >= 2"),
-        CheckConstraint("start_date < end_date")
-    )
-
-    id = Column(INT, primary_key=True)
-    title = Column(VARCHAR, nullable=False)
-    description = Column(TEXT)
-    start_date = Column(TIMESTAMP, nullable=False)
-    end_date = Column(TIMESTAMP, nullable=False)
-    status = Column(PgEnum(StatusEnum, name="order_status", create_type=False), nullable=False)
-    project_id = Column(INT, ForeignKey(column=Projects.id, ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
-    executor_id = Column(INT, ForeignKey(column=UsersW.id, ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
-
-    projects = relationship(argument=Projects, foreign_keys=[project_id], back_populates="tasks")
-    usersw = relationship(argument=UsersW, foreign_keys=[executor_id], back_populates="tasks")
-
-
-engine = create_engine(url="postgresql://user12:a0XCZnQ6H@217.76.60.77:6666/user12")
-session_maker = sessionmaker(bind=engine)
+#
+# class Base(DeclarativeBase):
+#     ...
+#
+#
+# class Projects(Base):
+#     __tablename__ = "projects"
+#     __table_args__ = (
+#         CheckConstraint("length(name) >= 2"),
+#     )
+#
+#     id = Column(INT, primary_key=True)
+#     name = Column(VARCHAR(length=32), nullable=False, unique=True)
+#
+#     tasks = relationship(argument="Tasks", back_populates="projects")
+#
+#
+# class UsersW(Base):
+#     __tablename__ = "usersw"
+#
+#     id = Column(INT, primary_key=True)
+#
+#     tasks = relationship(argument="Tasks", back_populates="usersw")
+#
+#
+# class StatusEnum(Enum):
+#     done = "done"
+#     not_done = "not_done"
+#
+#
+# class Tasks(Base):
+#     __tablename__ = "tasks"
+#     __table_args__ = (
+#         CheckConstraint("length(title) >= 2"),
+#         CheckConstraint("length(description) >= 2"),
+#         CheckConstraint("start_date < end_date")
+#     )
+#
+#     id = Column(INT, primary_key=True)
+#     title = Column(VARCHAR, nullable=False)
+#     description = Column(TEXT)
+#     start_date = Column(TIMESTAMP, nullable=False)
+#     end_date = Column(TIMESTAMP, nullable=False)
+#     status = Column(PgEnum(StatusEnum, name="order_status", create_type=False), nullable=False)
+#     project_id = Column(INT, ForeignKey(column=Projects.id, ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
+#     executor_id = Column(INT, ForeignKey(column=UsersW.id, ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
+#
+#     projects = relationship(argument=Projects, foreign_keys=[project_id], back_populates="tasks")
+#     usersw = relationship(argument=UsersW, foreign_keys=[executor_id], back_populates="tasks")
+#
+#
+# engine = create_engine(url="postgresql://user12:a0XCZnQ6H@217.76.60.77:6666/user12")
+# session_maker = sessionmaker(bind=engine)
 
 # with session_maker() as session:
 #     response = session.scalars(statement=q)
@@ -85,7 +85,7 @@ session_maker = sessionmaker(bind=engine)
 
 
 
-from os import name
+# from os import name
 from sqlalchemy import (
     Column,
     INT,
@@ -211,58 +211,57 @@ from sqlalchemy.orm import selectinload, joinedload
 q = select(Tasks).join(Projects)
 q = q.filter(
     Tasks.status != "done"
-    Task.end_date != "06-03-"
-
+    # Tasks.end_date <= "2024-03-06 00:00:00"
 )
 
 print(q)
 
 
 
-from psycopg2 import connect
-from psycopg2._psycopg import cursor
-from psycopg2.extras import NamedTupleCursor
-
-with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
-    with conn.cursor() as cur: # type: cursor
-        # cur.execute("""
-        #     INSERT INTO projects (name) VALUES (%s), (%s), (%s)
-        # """, ("pj_1", "pj_2", "pj_3")
-        #     )
-
-        cur.execute("""
-            SELECT * FROM projects
-        """)
-        for result in cur:
-            print(result)
-
-with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
-    with conn.cursor() as cur: # type: cursor
-        # cur.execute("""
-        #     INSERT INTO usersw (id) VALUES (%s), (%s), (%s)
-        # """, (1, 2, 3)
-        #     )
-
-        cur.execute("""
-            SELECT * FROM usersw
-        """)
-        for result in cur:
-            print(result)
-
-
-with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
-    with conn.cursor() as cur: # type: cursor
-        # cur.execute("""
-        #     INSERT INTO tasks (title, description, start_date, end_date, project_id, executor_id) VALUES (%s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s)
-        # """, ("task_1", "dododo", "2024-03-01 00:00:00", "2024-03-03 00:00:00", 1, 1, "task_2", "lalala", "2024-02-01 00:00:00", "2024-03-17 00:00:00", 2, 2, "task_3", "bebebe", "2024-02-15 00:00:00", "2024-02-29 00:00:00", 3, 3)
-        #     )
-
-        # cur.execute("""
-        #     SELECT * FROM tasks
-        # """)
-
-        for result in cur:
-            print(result)
+# from psycopg2 import connect
+# from psycopg2._psycopg import cursor
+# from psycopg2.extras import NamedTupleCursor
+#
+# with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
+#     with conn.cursor() as cur: # type: cursor
+#         # cur.execute("""
+#         #     INSERT INTO projects (name) VALUES (%s), (%s), (%s)
+#         # """, ("pj_1", "pj_2", "pj_3")
+#         #     )
+#
+#         cur.execute("""
+#             SELECT * FROM projects
+#         """)
+#         for result in cur:
+#             print(result)
+#
+# with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
+#     with conn.cursor() as cur: # type: cursor
+#         # cur.execute("""
+#         #     INSERT INTO usersw (id) VALUES (%s), (%s), (%s)
+#         # """, (1, 2, 3)
+#         #     )
+#
+#         cur.execute("""
+#             SELECT * FROM usersw
+#         """)
+#         for result in cur:
+#             print(result)
+#
+#
+# with connect(dsn="postgres://user12:a0XCZnQ6H@217.76.60.77:6666/user12", cursor_factory=NamedTupleCursor) as conn:
+#     with conn.cursor() as cur: # type: cursor
+#         # cur.execute("""
+#         #     INSERT INTO tasks (title, description, start_date, end_date, project_id, executor_id) VALUES (%s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s)
+#         # """, ("task_1", "dododo", "2024-03-01 00:00:00", "2024-03-03 00:00:00", 1, 1, "task_2", "lalala", "2024-02-01 00:00:00", "2024-03-17 00:00:00", 2, 2, "task_3", "bebebe", "2024-02-15 00:00:00", "2024-02-29 00:00:00", 3, 3)
+#         #     )
+#
+#         # cur.execute("""
+#         #     SELECT * FROM tasks
+#         # """)
+#
+#         for result in cur:
+#             print(result)
 
 
 
